c96f0234e86d4ff563d9561aab746e18cec9e5ea
Luis Miguel Acevedo <luisxcam@gmail.com>
2018-04-16 12:57:44 -0400

    Added service model
    Added a service to code all of the server requests on it

--- js/app.js
+++ js/app.js
@@ -2,26 +2,33 @@
 
 (function () {
     angular.module('mainApp', ['appControllers', 'ngRoute'])
+        .service('ApiRequestsService', ApiRequestsService)
         .config(function ($routeProvider, $locationProvider) {
             $locationProvider.hashPrefix('');
 
             $routeProvider.when('/addproducts/', {
                 controller: 'addProductCtrl',
                 templateUrl: 'modals/addproducts.html',
-                access: {restricted: true},
-                css:'css/style.css'
+                access: {
+                    restricted: true
+                },
+                css: 'css/style.css'
             });
             $routeProvider.when('/searchproducts/', {
                 controller: 'searchProduct',
                 templateUrl: 'modals/searchproducts.html',
-                access: {restricted: true},
-                css:'css/style.css'
+                access: {
+                    restricted: true
+                },
+                css: 'css/style.css'
             });
             $routeProvider.otherwise({
                 controller: 'pieCtrl',
                 templateUrl: 'modals/piechart.html',
-                access: {restricted: true},
-                css:'css/style.css'
+                access: {
+                    restricted: true
+                },
+                css: 'css/style.css'
             });
 
         });
@@ -30,22 +37,28 @@
         .controller('pieCtrl', PieCtrl)
         .controller('addProductCtrl', AddProductCtrl);
 
-    function PieCtrl($scope,$http) {
-        $scope.getRequest = function(){
-            $http.get('https://api.github.com/users/luisxcam').then(function(response){
-                console.log(response.data);
-            });
-        }
 
-        $scope.getRequest();
+    function PieCtrl(ApiRequestsService, $scope) {
+        console.log('calling example');
+        ApiRequestsService.example();
     }
 
     function AddProductCtrl() {
         console.log('add product loaded');
     }
 
-    function SearchProduct(){
+    function SearchProduct() {
         console.log('search for product');
+    }
+
+    function ApiRequestsService($http) {
+        var that = this;
+
+        that.example = function () {
+            $http.get('https://api.github.com/users/luisxcam').then(function (response) {
+                console.log(response.data);
+            });
+        }
     }
 })();
 
