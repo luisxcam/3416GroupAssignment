b832df872f88c0ce35897ef2dffc385a8e26711f
Luis Miguel Acevedo <luisxcam@gmail.com>
2018-04-15 20:12:04 -0400

    Adding angular groundwork
    Made the index an angular app. Added the lib to the project. Added the ng-app directive to the body of the file

--- js/app.js
+++ js/app.js
@@ -1,18 +1,21 @@
-google.charts.load("current", {
-    packages: ["corechart"]
-});
-google.charts.setOnLoadCallback(drawChart);
+// TODO: Need to check how exactly this chart works or come up with another framework
+// google.charts.load("current", {
+//     packages: ["corechart"]
+// });
+// google.charts.setOnLoadCallback(drawChart);
 
-function drawChart(data) {
-    var data = google.visualization.arrayToDataTable([
-        ['Products', 'Amount'],
-        ['Available Products', 11],
-        ['Used Products', 7]
-    ]);
-    var options = {
-        title: 'Stock Details',
-        pieHole: 0,
-    };
-    var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-    chart.draw(data, options);
-}
+// function drawChart(data) {
+//     var data = google.visualization.arrayToDataTable([
+//         ['Products', 'Amount'],
+//         ['Available Products', 11],
+//         ['Used Products', 7]
+//     ]);
+//     var options = {
+//         title: 'Stock Details',
+//         pieHole: 0,
+//     };
+//     var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+//     chart.draw(data, options);
+// }
+
+angular.module('mainApp',[]);
